a5ff3a6ad68bace0b7e31a3b38e16d7f3dcba06d
Kerim Ovezmyradov <ovezmuradov1208@gmail.com>
2026-03-16 19:04:48 +0800

    Implement collision detection to the car

--- car.js
+++ car.js
@@ -17,9 +17,11 @@ class Car{
     }
 
     update(roadBorders) {
-        this.#move();
-        this.polygon = this.#createPolygon();
-        this.damaged = this.#assessDamage(roadBorders);
+        if(!this.damaged) {
+            this.#move();
+            this.polygon = this.#createPolygon();
+            this.damaged = this.#assessDamage(roadBorders);
+        }
         this.sensor.update(roadBorders);
     }
 
@@ -97,6 +99,11 @@ class Car{
     }
 
     draw(ctx) {
+        if(this.damaged) {
+            ctx.fillStyle = "gray";
+        } else {
+            ctx.fillStyle = "black";
+        }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
         for(let i=1; i<this.polygon.length; i++) {
